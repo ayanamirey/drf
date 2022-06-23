0bac281a766f636bc3a8271a88c63b3658092afa
Sanjar <sanjar.ravshanbekov@gmail.com>
2022-06-23 21:49:51 +0500

    Viewsets и ModelViewSet

--- drfsite/urls.py
+++ drfsite/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from women.views import WomenAPIList, WomenAPIUpdate, WomenAPIDetailView
+from women.views import WomenViewSet
+from rest_framework import routers
+
+router = routers.SimpleRouter()
+router.register(r'women', WomenViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/womenlist/', WomenAPIList.as_view()),
-    path('api/v1/womenlist/<int:pk>/', WomenAPIUpdate.as_view()),
-    path('api/v1/womendetail/<int:pk>/', WomenAPIDetailView.as_view()),
+    path('api/v1/', include(router.urls)), # http://127.0.0.1:8000/api/v1/women/
+    # path('api/v1/womenlist/', WomenViewSet.as_view({'get': 'list'})),
+    # path('api/v1/womenlist/<int:pk>/', WomenViewSet.as_view({'put': 'update'})),
 ]
