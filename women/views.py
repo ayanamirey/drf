5b1b611087e8f835d7aa11517010e6a8cb85b09c
Sanjar <sanjar.ravshanbekov@gmail.com>
2022-06-25 20:37:32 +0500

    Аутентификация по токенам. Пакет Djoser

--- women/views.py
+++ women/views.py
@@ -1,8 +1,10 @@
 from rest_framework import generics
+from rest_framework.authentication import TokenAuthentication
+
 from women.models import Women, Category
 from women.permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
 from women.serializers import WomenSerializer
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 
 class WomenAPIList(generics.ListCreateAPIView):
@@ -14,8 +16,8 @@ class WomenAPIList(generics.ListCreateAPIView):
 class WomenAPIUpdate(generics.RetrieveUpdateAPIView):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
-    permission_classes = (IsOwnerOrReadOnly,)
-
+    permission_classes = (IsAuthenticated,)
+    # authentication_classes = (TokenAuthentication,)
 
 class WomenAPIDestroy(generics.RetrieveDestroyAPIView):
     queryset = Women.objects.all()
